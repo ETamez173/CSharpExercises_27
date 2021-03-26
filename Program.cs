bafeb524d191921ccca4ce4cb79a80bcad44c8bc
ETamez173 <56414835+ETamez173@users.noreply.github.com>
2021-03-25 20:09:41 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+// Object Methods | C# | Tutorial 27
+
+namespace CSharpExercises_27
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // two objects created in the Student class
+
+            Student student1 = new Student("Jim", "Business", 2.8);
+            Student student2 = new Student("Pam", "Art", 3.6);
+
+            // this uses the object method within the class to determine
+            // if gpa qualifies for honors
+            Console.WriteLine(student1.HasHonors());
+            Console.WriteLine(student2.HasHonors());
+
+            // this uses the object method within the class to determine
+            // if the major is Art
+            Console.WriteLine(student1.HasArt());
+            Console.WriteLine(student2.HasArt());
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Student.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharpExercises_27
+{
+    class Student
+    {
+        public string name;
+        public string major;
+        public double gpa;
+
+        // constructor to create student
+        public Student(string aName, string aMajor, double aGpa)
+        {
+            name = aName;
+            major = aMajor;
+            gpa = aGpa;
+        }
+
+        // object method to find if student has honors
+        public bool HasHonors()
+        {
+            //gpa = 90.4;
+
+            if(gpa >= 3.5)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        // object method to find if student is an Art major
+        public bool HasArt()
+        {
+            
+
+            if (major == "Art")
+            {
+                return true;
+            }
+            return false;
+        }
+
+
+
+
+
+    }
+}
